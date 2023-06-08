8371d416de89b00c8502ea94d18a719fdb11726c
olsikule <olsi.kule@gmail.com>
2023-06-08 11:06:30 -0400

    final updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, Injector, OnInit} from '@angular/core';
-import {Course} from './model/course';
-import {Observable} from 'rxjs';
-import {AppConfig, CONFIG_TOKEN} from './config';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
-import {CoursesService} from './courses/courses.service';
-import {createCustomElement} from '@angular/elements';
-import {CourseTitleComponent} from './course-title/course-title.component';
-
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -15,37 +12,16 @@ import {CourseTitleComponent} from './course-title/course-title.component';
 })
 export class AppComponent implements OnInit {
 
-    courses: Course[] = COURSES;
 
-    coursesTotal = this.courses.length;
+  courses = COURSES;
 
-    constructor(
-        private coursesService: CoursesService,
-        @Inject(CONFIG_TOKEN) private config: AppConfig,
-        private injector: Injector) {
+  constructor() {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+  }
 
-        const htmlElement = createCustomElement(CourseTitleComponent, {injector:this.injector});
-
-        customElements.define('course-title', htmlElement);
-
-    }
-
-    onEditCourse() {
-
-            this.courses[1].category = 'ADVANCED';
-
-    }
-
-    save(course: Course) {
-        this.coursesService.saveCourse(course)
-            .subscribe(
-                () => console.log('Course Saved!')
-            );
-    }
 
 
 }
